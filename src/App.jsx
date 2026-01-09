20451a4b01681a18336b3adf6ff9d693067686a6
keen coding <medhanyetsegay96@gmail.com>
2026-01-09 13:27:40 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
-import React from "react";
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
-
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollSmoother } from "gsap/ScrollSmoother";
 import HorizontalScroll from "./components/HorizontalScroll";
-gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother);
-
-ScrollSmoother.create({
-  smooth: 1,
-  effects: true,
-  smoothTouch: 0.1,
-});
 
 export default function App() {
   return (
     <>
-      <div id="smooth-wrapper">
-        <div id="smooth-content">
-          <HorizontalScroll />
-        </div>
-      </div>
+      <HorizontalScroll />
     </>
   );
 }
